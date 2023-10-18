63f888edb4c924d2175684f0536e062ee7585ae0
Kevin327 <1779894826@qq.com>
2023-10-18 13:44:28 +0800

    genshin commit

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -43,7 +43,9 @@ export default function LandingPage() {
             <div className="icon-text-container">
                 <div className="icon-text-container" style={iconContainerStyle}>
                     <div className="icon-text" style={iconOutStyle}>
-                        <img src={A5} alt="Icon 1" className="icon-1" style={iconStyle} />
+                        <a href={"https://genshin.mihoyo.com/en"} target="_blank" rel="noreferrer" style={aStyle}>
+                        <img src={A5} alt="Icon 1" className="icon-1" style={iconStyle2} />
+                        </a>
                         <p>Genshin</p>
                     </div>
                 </div>
@@ -83,11 +85,15 @@ const wordStyle = {
 const iconStyle = {
     width: '90%', // 调整图像宽度
     height: '90%', // 调整图像高度
-    minWidth: '90%',
-    minHeight: '90%',
-    margin: '60px 60px 0', // 设置图像之间的间距
+    margin: '30px 60px 0', // 设置图像之间的间距
     objectFit: 'fill',
 };
+const iconStyle2 = {
+    width: '90%', // 调整图像宽度
+    height: '90%', // 调整图像高度
+    objectFit: 'fill',
+    margin: '30px 0px 1px'
+}
 const iconOutStyle = {
     width: '230px', // 调整图像宽度
     height: '230px', // 调整图像高度
@@ -100,6 +106,14 @@ const iconOutStyle = {
     objectFit: 'cover',
     backgroundAttachment: 'fixed',
     backgroundSize: 'cover',
+}
+
+const aStyle = {
+    margin: 0,
+    padding: 0,
+    textDecoration: 'none',
+    display: 'inline-block',
+    border: 'none',
 }
 
 const iconContainerStyle = {
